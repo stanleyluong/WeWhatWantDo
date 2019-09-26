9f8e8a45ad3a645c44dcf74f9c98545e6dbc408a
github username <github email address>
2019-09-26 11:53:09 -0700

    updated group api stuff

--- what-we-do-backend/app/models/group.rb
+++ what-we-do-backend/app/models/group.rb
@@ -1,58 +1,46 @@
 class Group < ApplicationRecord
     has_many :invites
     has_many :users, through: :invites
-
-    def suggestions
-        #sample content from each user and throw it into function
-        #run fetch and return output of fetch statement
+    def suggestions #sample content from each user and throw it into function. run fetch and return output of fetch statement
         query = []
+        type = []
         self.users.each do |user|
             query << user.likes.sample(3)
         end
         query = query.flatten
-        type = []
         query.each do |like| 
             type << like.type
         end
         type = type.flatten
         type.uniq
-        
-        fetch(query,type)
+        return makeUrl(query,type)
     end
-
-    # https://tastedive.com/api/similar?q=eminem%2C+pulp+fiction
-
-    # https://tastedive.com/api/similar?k=346710-BrittanF-PHC42GKW&q=dark+crystal%2C+labyrinth&type=movies%2C+shows&verbose=1&limit=20
-    def fetch(query, type)
-        url = 'https://tastedive.com/api/similar?k=346710-BrittanF-PHC42GKW&q='+query+type
-        response = RestClient.get(url)
-        $parsed_response = JSON.parse(response)
-    end
-
     def translator(array)
         array.map! do |string| 
-                if  string != array[-1] then
-                    string = string.split(" ")
-                    string.map! do |word|
+            if  string != array[-1] then
+                string = string.split(" ")
+                string.map! do |word|
                     word != string[-1] ? word+"+" : word+"%2C+"
-                    end
-                else
-                    string = string.split(" ")
-                    string.map! do |word|
+                end
+            else
+                string = string.split(" ")
+                string.map! do |word|
                     word!= string[-1] ? word+"+" : word
                 end
             end
         end
         array = array.flatten.join("")
     end
-
     def makeUrl(query, type)
         key = '346710-BrittanF-PHC42GKW'
         url = "https://tastedive.com/api/similar?k=#{key}&q="+translator(query)+"&type="+translator(type)
+        fetch(url)
     end
-
-    # query = ['red hot chili peppers','pulp fiction','burger king', "dark crystal","labyrinth"]
-    # type = ['movies','shows']
-    # makeUrl(query,type)
-    
+    def fetch(url)
+        response = RestClient.get(url)
+        $parsed_response = JSON.parse(response)
+    end
 end
+# query = ['red hot chili peppers','pulp fiction','burger king', "dark crystal","labyrinth"]
+# type = ['movies','shows']
+# makeUrl(query,type)  
