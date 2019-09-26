32161a75ff6f5551ab8b5b679a0dc01b9cc45b9d
Nick Dunlap <nwdunlap17@gmail.com>
2019-09-26 13:26:13 -0700

    user search bar

--- what-we-do-frontend/src/App.js
+++ what-we-do-frontend/src/App.js
@@ -23,6 +23,9 @@ class App extends Component{
     })
   }
 
+
+
+
   render() {
     return (
     <div className="App">
--- what-we-do-frontend/src/components/InviteBar.js
+++ what-we-do-frontend/src/components/InviteBar.js
@@ -4,15 +4,13 @@ import React, {Component} from 'react'
 export default class InviteBar extends Component{
     constructor(props){
         super(props)
-        this.state = {search: ''}
+        this.state = {search: '', foundUsers: []}
     }
 
-    upDateSearch = (e) => {
+    upDateSearch = (e) => { 
         this.setState({search: e.target.value})
-    }
 
-    foundUsers = () => {
-        if (this.state.search.length > 0){
+        if (e.target.value.length > 0){
             fetch(this.props.BackendURL+'/usersearch',{
                 method:"POST",
                 headers:{
@@ -20,11 +18,33 @@ export default class InviteBar extends Component{
                     'Accept':'application/json'
                 },
                 body: JSON.stringify({
-                    search: this.state.search
+                    search: e.target.value
                 })
             })
-            .then(console.log)
+            .then(response => response.json())
+            .then(json => {
+                this.setState({foundUsers: json})
+            })
         }
+    }
+
+    makeUserList = (json) => {
+        if (json.length > 0 && this.state.search.length > 0){
+            let list = json.map(element => {
+                return <li><button value={element} onClick={this.handleButton}>{element.username}</button></li>
+            });
+            let renderList = (
+            <ul>
+                <li>A list</li>
+                {list}
+            </ul>
+            )
+            return renderList
+        }
+    }
+
+    handleButton = (e) => {
+        this.props.return(e.target.value)
     }
 
     render(){
@@ -33,9 +53,9 @@ export default class InviteBar extends Component{
                 <label>Search Users:</label>
                 <input type='text' value={this.state.search}
                     onChange={e=>this.upDateSearch(e)}/>
-                <ul>
-                    {this.foundUsers()}
-                </ul>
+
+                    {this.makeUserList(this.state.foundUsers)}
+
             </form>
         )
     }
