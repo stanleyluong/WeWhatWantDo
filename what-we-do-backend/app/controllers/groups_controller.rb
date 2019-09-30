273ab5301674f697e1686b28fd46bc034cb95af6
Nick Dunlap <nwdunlap17@gmail.com>
2019-09-30 16:09:36 -0700

    sugjuiced

--- what-we-do-backend/app/controllers/groups_controller.rb
+++ what-we-do-backend/app/controllers/groups_controller.rb
@@ -13,7 +13,6 @@ class GroupsController < ApplicationController
     end
 
     def create
-        @user = User.find(session[:current_user_id])
         @group = Group.new(group_params)
         if @group.save
             @user.groups << @group 
@@ -32,13 +31,13 @@ class GroupsController < ApplicationController
 
     def suggest
         @suggestions = @group.getSuggestions(params[:types])
-        render :json => @suggestions
+        
+        render :json => @suggestions['Similar']['Results']
     end
    
     private
 
     def grab_group
-        @user = User.find(session[:current_user_id])
         @group = Group.find(params[:id])
     end
 
