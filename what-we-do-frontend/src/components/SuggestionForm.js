089706be05f6aa9eb098b6790446819417e9ab97
github username <github email address>
2019-10-02 11:25:29 -0700

    polishing

--- what-we-do-frontend/src/components/SuggestionForm.js
+++ what-we-do-frontend/src/components/SuggestionForm.js
@@ -5,15 +5,17 @@ import SuggestionCard from "./SuggestionCard";
 export default class SuggestionForm extends Component {
   state = {
     type: null,
-    suggestion: []
+    allSuggestions: [],
+    suggestion: [],
+    sliceCounter: 0
   };
 
   handleChange = (e, { value }) => {
-    this.setState({ type: value });
+    this.setState({ type: value, sliceCounter: 0 });
   };
 
   getGroupSuggestion = () => {
-      console.log("suggestion types",this.state.type)
+    console.log("suggestion types", this.state.type);
     fetch(
       this.props.BackendURL + `/groups/${this.props.currentGroup.id}/suggest`,
       {
@@ -30,80 +32,105 @@ export default class SuggestionForm extends Component {
     )
       .then(res => res.json())
       .then(async data => {
-          console.log(data)
+        console.log(data);
         this.setState({
-          suggestion: data.slice(0, 4)
+          allSuggestions: data,
+          suggestion: data.slice(
+            this.state.sliceCounter,
+            this.state.sliceCounter + 4
+          )
         });
       })
+      .then(response =>
+        this.setState({
+          sliceCounter: 1
+        })
+      );
   };
 
   generateSuggestionCards = () => {
     if (this.state.suggestion.length !== 0) {
-      return this.state.suggestion.map((item, index) => (
-          <div>
-         <SuggestionCard
-          key={index}
-          itemData={item}
-        />
+      return this.state.allSuggestions.slice((this.state.sliceCounter - 1), (this.state.sliceCounter + 3)).map((item, index) => (
+        <div>
+          <SuggestionCard key={index} itemData={item} />
         </div>
-      ))
-      
+      ));
     }
   };
+
+  generateSuggestionButton = () => {
+    if (this.state.sliceCounter <= 17) {
+      return this.state.sliceCounter === 0 ? (
+        <Form.Button onClick={() => this.getGroupSuggestion()}>
+          Get Group Suggestion!
+        </Form.Button>
+      ) : (
+        <Form.Button onClick={() => this.incrementSliceCounter()}>
+          Get More Suggestions!
+        </Form.Button>
+      );
+    } else {
+        this.setState({
+            sliceCounter: 1
+        })
+    }
+  };
+
+  incrementSliceCounter = () => {
+      this.setState(prevState =>{
+          return {sliceCounter: (prevState.sliceCounter +4)}
+      })
+  }
 
   render() {
     const { value } = this.state;
     return (
       <div>
+        <label>Media type:</label>
+
         <Form>
+
+
+    
           <Form.Group inline widths="equal">
-            <label>Media type:</label>
             <Form.Checkbox
-              label="Movies"
-              value="movie"
-              checked={value === "movies"}
+              label='Movies'
+              value='movie'
+              checked={this.state.type === 'movie'}
               onChange={this.handleChange}
             />
             <Form.Checkbox
               label="TV Shows"
               value="show"
-              checked={value === "shows"}
+              checked={this.state.type === "show"}
               onChange={this.handleChange}
             />
             <Form.Checkbox
               label="Books"
               value="book"
-              checked={value === "books"}
+              checked={this.state.type === "book"}
               onChange={this.handleChange}
             />
             <Form.Checkbox
               label="Authors"
               value="author"
-              checked={value === "authors"}
+              checked={this.state.type === "author"}
               onChange={this.handleChange}
             />
             <Form.Checkbox
               label="Music"
               value="music"
-              checked={value === "music"}
+              checked={this.state.type === "music"}
               onChange={this.handleChange}
             />
             <Form.Checkbox
               label="Games"
               value="game"
-              checked={value === "games"}
-              onChange={this.handleChange}
-            />
-            <Form.Checkbox
-              label="Podcasts"
-              value="podcast"
-              checked={value === "podcasts"}
+              checked={this.state.type === "game"}
               onChange={this.handleChange}
             />
           </Form.Group>
-          <Form.Button onClick={() => this.getGroupSuggestion()}>
-            Get Group Suggestion!
-          </Form.Button>
+          {this.generateSuggestionButton()}
         </Form>
 
         <div class="suggestionContainer">{this.generateSuggestionCards()}</div>
